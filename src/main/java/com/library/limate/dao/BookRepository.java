649604a44caa267610eaa5b051977663a5039b70
Torao <bahoann79@gmail.com>
2024-10-18 10:40:29 +0700

    Initial commit

---
+++ src/main/java/com/library/limate/dao/BookRepository.java
@@ -0,0 +1,7 @@
+package com.library.limate.dao;
+
+import com.library.limate.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
